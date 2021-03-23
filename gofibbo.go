c356d397994f5038104a8c868dc708c4da5aff77
Amnesix <jm.technique@free.fr>
2021-03-23 21:49:47 +0100

    Optimisation par memoization

--- gofibbo.go
+++ gofibbo.go
@@ -1,8 +1,13 @@
 package gofibbo
 
+var memo [100]int64
+
 func Fibbo(n int64) int64 {
 	if n < 3 {
 		return 1
 	}
-	return Fibbo(n-1) + Fibbo(n-2)
+	if memo[n] == 0 {
+		memo[n] = Fibbo(n-1) + Fibbo(n-2)
+	}
+	return memo[n]
 }
